6a5bfaa67ca61d22b893c2b91f69cf22ab50d543
Arief Daffa <ariefkeren76@yahoo.com>
2022-03-28 15:24:01 +0700

    update Homework

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Gif = ({ gif }) => {
 	return (
-		<div>
+		<div style={{ padding: '16px 0' }}>
 			<div>{gif.title}</div>
 			<img src={gif.url} alt="" />
 		</div>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import Form from '../../components/form';
+// import Form from '../../components/form';
 import Gif from '../../components/Gif';
+import { data } from '../../data/data';
 
-const Home = ({ gif }) => {
+const Home = () => {
 	return (
 		<>
-			<Gif gif={gif} />
-			<img
+			{/* <img
 				src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif"
 				alt=""
-			/>
-			<Form />
+				/>
+			<Form /> */}
+
+			{data
+				.filter((elements) => elements.rating === 'g')
+				.map((data) => (
+					<Gif gif={data} key={data.id} />
+				))}
 		</>
 	);
 };
