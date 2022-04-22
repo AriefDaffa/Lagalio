ccc72fce2586497bc220744a9a0f94a87a573e5a
Arief Daffa <ariefkeren76@yahoo.com>
2022-04-22 23:43:43 +0700

    fix: renaming login component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 export { default as Home } from './Home';
-export { default as Login } from './Logins';
+export { default as Login } from './Login';
 export { default as Expired } from './Expired';
 export { default as ListPlaylist } from './ListPlaylist';
 export { default as Playlist } from './Playlist';
