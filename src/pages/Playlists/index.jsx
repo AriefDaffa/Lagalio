6d87cfe78c8f016d619264e33289527bde35e593
Arief Daffa <ariefkeren76@yahoo.com>
2022-04-22 23:41:52 +0700

    fix naming bug

--- src/pages/Playlists/index.jsx
+++ src/pages/Playlists/index.jsx
@@ -1,45 +1,67 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { URLSearchParams } from 'url';
-import { Container, Navbar } from '../../components';
+import { Loading, Navbar } from '../../components';
 import { resetToken } from '../../redux/slice/token-slice';
-import { RootState } from '../../redux/store';
-import { PlaylistDetail } from './Layout';
-import PlaylistTracks from './Layout/PlaylistTracks';
+import { PlaylistDetail, PlaylistTracks } from '../../Layout/Playlist';
 
 const index = () => {
-	let { id } = URLSearchParams();
+	let { id } = useParams();
 	const [data, setData] = useState({});
 	const dispatch = useDispatch();
 	const token = useSelector((state) => state.token.token);
 
 	useEffect(() => {
-		const url = `https://api.spotify.com/v1`;
-		const header = {
-			Authorization: 'Bearer ' + token,
-		};
-		fetch(`${url}/playlists/${id}`, {
-			headers: header,
-		})
-			.then((res) => res.json())
-			.then((res) => {
-				if (typeof res.error === 'object') {
-					dispatch(resetToken(true));
-				} else {
-					setData(res);
-				}
+		const request = async () => {
+			const url = `https://api.spotify.com/v1`;
+			const header = {
+				Authorization: 'Bearer ' + token,
+			};
+			await fetch(`${url}/playlists/${id}`, {
+				headers: header,
 			})
-			.catch((err) => console.error(err));
+				.then((res) => res.json())
+				.then((res) => {
+					if (typeof res.error === 'object') {
+						dispatch(resetToken(true));
+					} else {
+						setData(res);
+					}
+				})
+				.catch((err) => console.error(err));
+		};
+		request();
 	}, []);
 
 	return (
 		<>
 			<Navbar />
-			<Container>
-				<PlaylistDetail data={data} />
-				<PlaylistTracks />
-			</Container>
+			{typeof data.images === 'undefined' ? (
+				<div
+					style={{
+						height: '100vh',
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'center',
+						background:
+							'linear-gradient(179.92deg, #273053 0.07%, #242035 134.98%)',
+					}}
+				>
+					<Loading />
+				</div>
+			) : (
+				<div
+					style={{
+						minHeight: '100vh',
+						background:
+							'linear-gradient(179.92deg, #273053 0.07%, #242035 134.98%)',
+					}}
+				>
+					<PlaylistDetail data={data} />
+					<PlaylistTracks data={data} />
+					{/* <ButtonSection /> */}
+				</div>
+			)}
 		</>
 	);
 };
